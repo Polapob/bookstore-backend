1264fbc6888c88b026dbdb428c06d9b7f1cffed5
Polapob <71330294+Polapob@users.noreply.github.com>
2023-04-13 18:31:51 +0700

    fix: update Program.cs

---
+++ Program.cs
@@ -0,0 +1,30 @@
+using bookstore_backend.Contexts;
+
+DotNetEnv.Env.Load(".env.development");
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<UserContext>();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
